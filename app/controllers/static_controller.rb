b2edaba233e9aecc545fe231b91111a6905a972d
Melih Omay <ioooomer@gmail.com>
2012-07-10 00:26:41 +0200

    mini

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def home
+  end
+
+  def imprint
+  end
+
+  def info
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Bim::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+
+  match "static/:action" => "static#:action", as: :static
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -48,7 +51,7 @@ Bim::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static#home'
 
   # See how all your routes lay out with "rake routes"
 
