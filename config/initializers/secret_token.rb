879009ec4df396b5551842bf32df6d5641a5bd96
Melih Omay <ioooomer@gmail.com>
2012-07-09 23:38:14 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bim::Application.config.secret_token = '1a1da68087272fb64277a30cf33003a133c7766d208ab8d9e4d744ef44aa090abe24c5ba5f306df8d7cc1fd7ac4b77ebbc8c1b6b746127c5f81c28c08f920b68'
