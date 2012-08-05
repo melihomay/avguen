57a702ceb938e70aa9812b9042f59c00c056d989
Melih Omay <ioooomer@gmail.com>
2012-08-06 00:02:10 +0200

    flash eingebunden

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -9,5 +9,8 @@ class StaticController < ApplicationController
   end
 
   def gazete
+    @layout = 'layout=http://192.168.178.82:3000/static/flash/layout.xml'
+    @documentId = params[:id] 
+
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def asset_url asset
+    "#{request.protocol}#{request.host_with_port}#{asset_path(asset)}"
+  end
+
 end
